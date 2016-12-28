acd48bb3d1a5fd8dfce3a944a6692ae44afa7c44
Arron Mayers <arronbq@gmail.com>
2016-12-28 10:20:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+        Book.find(function(err,books){
+            if(err)
+            console.log(err)
+            else
+            res.json(books);
+        });        
+    });
+
+app.use('/api', bookRouter);
+
+
+app.get('/', function(req,res){
+    res.send('welcome to my API!');
+});
+
+app.listen(port, function(){
+    console.log('Running on PORT:', port);
+})
